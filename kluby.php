d036cfb12ff96256a5a706dc0165dcba4f4cd11b
Matiquel <Mateusz.pyl@interia.pl>
2017-12-14 08:58:13 +0100

    Add files via upload

---
+++ kluby.php
@@ -0,0 +1,60 @@
+<?php
+include("header.php");
+
+?>
+
+<div class="card-body center">
+<div class="col-lg-6">
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Wyszukaj klub..." aria-label="Search for...">
+      <span class="input-group-btn"  style="margin-left: 1rem">
+        <button class="btn btn-secondary" type="button">Szukaj</button>
+      </span>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+
+  <div class="card-deck">
+      <div class="card" style="margin-top: 20px;">
+        <div class="col-sm-6">
+        <div class="card-body">
+          <div class="row">
+           <div class="col-md-6">
+             <img src="http://livingstyle.pl/media/products/7c161f96be/images/thumbnail/big_lechia-gdansk-logo-png.png?lm=1465403079" class="img-responsive rounded" alt="png" width="64" height="64">
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+
+  <div class="card" style="margin-top: 20px;">
+    <div class="col-sm-6">
+    <div class="card-body">
+       <div class="row">
+         <div class="col-md-6">
+           <img src="http://livingstyle.pl/media/products/7c161f96be/images/thumbnail/big_lechia-gdansk-logo-png.png?lm=1465403079" class="img-responsive rounded" alt="png" width="64" height="64">
+   </div>
+ </div>
+</div>
+</div>
+</div>
+
+<div class="card" style="margin-top: 20px;">
+  <div class="col-sm-6">
+  <div class="card-body">
+     <div class="row">
+       <div class="col-md-6">
+         <img src="http://livingstyle.pl/media/products/7c161f96be/images/thumbnail/big_lechia-gdansk-logo-png.png?lm=1465403079  " class="img-responsive rounded" alt="png" width="64" height="64">
+ </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<?php include("footer.php");?>
---
+++ pilkarze.php
@@ -0,0 +1,76 @@
+<?php
+include("header.php");
+
+?>
+
+
+<div class="card-body center">
+<div class="col-lg-6">
+    <div class="input-group">
+      <input type="text" class="form-control" placeholder="Wyszukaj zawodnika..." aria-label="Search for...">
+      <span class="input-group-btn" style="margin-left: 1rem">
+        <button class="btn btn-secondary" type="button">Szukaj</button>
+      </span>
+    </div>
+  </div>
+  </div>
+</div>
+
+
+  <div class="card-deck">
+      <div class="card" style="margin-top: 20px;">
+        <div class="col-sm-6">
+        <div class="card-body">
+          <div class="row">
+           <div class="col-md-6">
+             <img src="http://futhead.cursecdn.com/static/img/18/players/211110.png" class="img-responsive rounded" alt="png" width="64" height="64">
+      </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+
+  <div class="card" style="margin-top: 20px;">
+    <div class="col-sm-6">
+    <div class="card-body">
+       <div class="row">
+         <div class="col-md-6">
+           <img src="http://futhead.cursecdn.com/static/img/18/players/211110.png" class="img-responsive rounded" alt="png" width="64" height="64">
+   </div>
+ </div>
+</div>
+</div>
+</div>
+
+<div class="card" style="margin-top: 20px;">
+  <div class="col-sm-6">
+  <div class="card-body">
+     <div class="row">
+       <div class="col-md-6">
+         <img src="http://futhead.cursecdn.com/static/img/18/players/211110.png" class="img-responsive rounded" alt="png" width="64" height="64">
+ </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+<!-- <div class="card-deck">
+    <div class="card" style="margin-top: 25px;">
+      <div class="card-body">
+        <div class="card-title">
+          <div class="col-md-6">
+            <img  src="http://futhead.cursecdn.com/static/img/18/players/211110.png" class="img-responsive rounded" alt="png" width="64" height="64">
+            <span style="margin-left: 10px;font-size: 20px;">Paulo Dybala</span>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div> -->
+
+
+
+<?php include("footer.php");?>
